9f1defb938e58f43ce0b6dba32886058fce88e23
reaganscofield <reaganscofieldkayembe@outlook.com>
2019-05-23 02:38:52 -0700

    initial committed

---
+++ Controllers/TestingController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using web_application.Models;
+
+namespace web_application.Controllers
+{
+    public class TestingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Testing()
+        {
+            List<Models.PersonModel> peope = new List<Models.PersonModel>();
+            peope.Add(new PersonModel { FirstName = "Reagan", LastName = "Scofield", Age = 34, IsAlive = true });
+            peope.Add(new PersonModel { FirstName = "Judith", LastName = "Scofield", Age = 28, IsAlive = true });
+            peope.Add(new PersonModel { FirstName = "Almeida", LastName = "Scofield", Age = 6, IsAlive = true });
+            peope.Add(new PersonModel { FirstName = "Aleah", LastName = "Scofield", Age = 2, IsAlive = true });
+
+            return View(peope);
+        }
+    }
+}
---
+++ Models/PersonModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web_application.Models
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set;  }
+        public int Age { get; set; } = 0;
+        public bool IsAlive { get; set; } = true;
+
+        public PersonModel()
+        {
+
+        }
+    }
+}
